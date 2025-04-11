e6156330bfb0b7fdc03de49f2e7a19ae5bd79c2b
zain <mr.zain903@gmail.com>
2025-04-11 23:16:56 +0500

    styling admin page

--- ecommerceadmin/lib/mongodb.js
+++ ecommerceadmin/lib/mongodb.js
@@ -1,10 +1,10 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-if (!process.env.MONGODB_URL) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URL"');
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-const uri = process.env.MONGODB_URL;
+const uri = process.env.MONGODB_URI;
 const options = {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -16,14 +16,12 @@ const options = {
 let client;
 
 if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable to persist client across module reloads
   if (!global._mongoClient) {
     global._mongoClient = new MongoClient(uri, options);
   }
   client = global._mongoClient;
 } else {
-  // In production mode, create a new MongoClient on every request
   client = new MongoClient(uri, options);
 }
 
-export default client;
+module.exports = client;
